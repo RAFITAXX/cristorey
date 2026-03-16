3c52e503c5fb71204fae3b535e9d1752e189bef2
TomasPUCV <tomas.p.nunez.c04@gmail.com>
2026-03-16 12:07:43 -0300

    Ejercicio 1

--- Lab 1.py
+++ Lab 1.py
@@ -1,1 +1,10 @@
+V = int(input("Voltaje es: "))
+I = int(input("Corriente es: "))
+
+Resistencia = V/I 
+Potencia = V*I 
+print(Resistencia,  Potencia)
+
+if Potencia > 1000: print("¡Peligro! Alta disipación de potencia detectada")
+else: print("Operación en rangos seguros")
 
